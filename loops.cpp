992160dac07ce9fb156efa0c82ae6c7dd78ded00
Yuvati mankar <yuvatimankar06@gmail.com>
2021-09-07 06:02:34 +0530

    added

---
+++ loops.cpp
@@ -0,0 +1,26 @@
+// Print 10 lines of output; each line i (where ) (1<=i<=10)contains the result  of n*i in the form:
+// n x i = result.
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main()
+{
+   int n;
+   cin>>n;
+   int result;
+   for(int i=1;i<=10;i++)
+   {
+      result=n*i; 
+      
+      cout<< n <<" "<<"x"<< " "<< i << " = "<<result<<endl;
+   } 
+    
+    
+    return 0;
+}
+
